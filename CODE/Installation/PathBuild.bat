83ae6a4e2fbc69f26797c3ffbc849b98099d462a
MAT-services <157519566+MAT-services@users.noreply.github.com>
2025-01-18 12:29:51 +0100

    Update PathBuild.bat

--- CODE/Installation/PathBuild.bat
+++ CODE/Installation/PathBuild.bat
@@ -1,4 +1,27 @@
+@echo off
+chcp 65001
+setlocal enabledelayedexpansion
+
+set n=1
+
 :a
-<%filePath% set /p path=
-md %path%
+set "filePath=Path%n%.ini"
+if not exist %filePath% goto error
+
+<%filePath% set /p InstallPath=
+
+if not exist "%InstallPath%" (
+    md "%InstallPath%"
+    echo Répertoire créé à : %InstallPath%
+) else (
+    echo Le répertoire %InstallPath% existe déjà.
+)
+
+set /a n+=1
+echo n=%n%
+
+goto f
+:error 
+echo FATAL ERROR
+:f
 pause
